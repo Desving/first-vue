854e59e68b0f302f6bd6feb0354aa1da34a6158e
Denis Shergin <shergin@intervolga.ru>
2018-09-13 16:31:13 +0300

    Установка

---
+++ components/AppLogo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="VueToNuxtLogo">
+    <input type="text" v-model="name">
+  </div>
+</template>
+
+<style>
+.VueToNuxtLogo {
+  display: inline-block;
+  position: relative;
+  overflow: hidden;
+  height: 180px;
+  width: 245px;
+}
+
+.Triangle {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 0;
+}
+</style>
+
---
+++ layouts/error.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <h1 v-if="error.statusCode === 404">А ну ушел отсюда</h1>
+        <h1 v-else>Произошла ошибка</h1>
+        <nuxt-link to="/">Вернуться на главную</nuxt-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['error'],
+        head: {
+            title: '404 ошибка'
+        }
+    }
+</script>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
+    h1 {
+        color: red;
+        margin: auto;
+    }
+
+</style>
